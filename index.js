87e4ece4dc0e1e9556e47a7cd531afe8ae8bec6d
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-08-25 00:21:46 +0530

    created user route to handle login and signup

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const path= require ("path");
-
+const userRoute = require("./routes/user");
+const { displayHomePage} = require("./controllers/handlePageRender");
 const app = express();
 const PORT = 8000;
 
 app.set("view engine" , "ejs");
 app.set("views" , path.resolve("./views"));
 
-app.get("/" , (req , res) =>{
-     res.render("home");
-});
+app.get("/" , displayHomePage);
+
+app.use("/user" , userRoute);
 
 app.listen(PORT , ()=> console.log(`server stared at PORT : ${PORT}`));
 
