9c22ffe8ea5aa11c79b99e2e1b0f18c78702c20b
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-08-25 00:24:15 +0530

    added hash functo to password to encrypt using node:crypto

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const {createHmac , randomBytes} = require("node:crypto");
+
 const {Schema , model} =require("mongoose");
 
 const userSchema = new Schema({
@@ -32,6 +34,21 @@ const userSchema = new Schema({
     timestamps : true,
 });
 
+ userSchema.pre("save" , function(next){
+    const user = this;
+
+    if(!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const incryptedPassword = createHmac("sha256" , salt)
+        .update(user.password)
+        .digest("hex");
+
+    this.salt=salt;
+    this.password=incryptedPassword;
+
+    next();
+ });
 
 const User = model("user" , userSchema);
 
