ba979bc846463ce40f793b9feb6360eecf4d2b46
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-08-25 17:23:57 +0530

    db connection

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const {handleConnecton} = require("./connections");
 const path= require ("path");
 const userRoute = require("./routes/user");
 const { displayHomePage} = require("./controllers/handlePageRender");
 const app = express();
 const PORT = 8000;
 
+handleConnecton("mongodb://127.0.0.1:27017/blog_application").then(console.log("DB connected successfully"));
+
 app.set("view engine" , "ejs");
 app.set("views" , path.resolve("./views"));
+
+app.use(express.urlencoded({extended : false}));
 
 app.get("/" , displayHomePage);
 
