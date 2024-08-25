88b945d3b459b7135e37be5b649fde88e1c02d1c
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-08-25 17:21:55 +0530

    added post routes for login and signup

--- routes/user.js
+++ routes/user.js
@@ -1,19 +1,20 @@
 const express = require("express");
 
+const {handleUserSignup , handleUserLogin} = require("../controllers/handleUser.js")
 const {displayLoginPage , displaySignupPage} =require("../controllers/handlePageRender");
+
 const router =express.Router();
 
 router.get("/signup" , displaySignupPage);
 
 router.get("/login" , displayLoginPage);
 
-router.post("/login" , handleLogin);
+router.post("/login" , handleUserLogin);
 
-router.post("/signup" , handleSignup);
+router.post("/signup" , handleUserSignup);
 
 
 module.exports = router;
-
 
 
 
