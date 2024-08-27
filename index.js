becc8227ec94b6bb952557f65a7c08065d348618
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-08-27 21:54:54 +0530

    cookie-parser used , middleware to check cookie

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cookeParser = require("cookie-parser");
 const {handleConnecton} = require("./connections");
 const path= require ("path");
 const userRoute = require("./routes/user");
 const { displayHomePage} = require("./controllers/handlePageRender");
+const cookieParser = require("cookie-parser");
+const { checkAuthenticationCookie } = require("./middleware/authentication");
 const app = express();
 const PORT = 8000;
 
@@ -12,7 +15,8 @@ app.set("view engine" , "ejs");
 app.set("views" , path.resolve("./views"));
 
 app.use(express.urlencoded({extended : false}));
-
+app.use(cookieParser());
+app.use(checkAuthenticationCookie("token"));
 app.get("/" , displayHomePage);
 
 app.use("/user" , userRoute);
