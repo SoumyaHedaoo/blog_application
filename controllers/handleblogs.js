2df03fca334968fe0a66965e447b49969e4845e6
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-09-24 14:39:08 +0530

    save blog in database

--- controllers/handleblogs.js
+++ controllers/handleblogs.js
@@ -1,11 +1,17 @@
-
+const Blog = require("../models/blog");
 const CreateNewBlog = (req , res)=>{
     return res.render("addblog" ,{ user : req.user});
 }
 
-const handleSubmitBlog = (req , res) => {
-    console.log(req.body);
-    return res.redirect('/');
+const handleSubmitBlog = async(req , res) => {
+    const{title , body} = req.body;
+    const blog = await Blog.create({
+        body,
+        title,
+        createdBy : req.user._id,
+        thumbnailImage : `uploads/${req.file.filename}`
+    });
+    return res.redirect(`/blog/${blog._id}`);
 }
 
 module.exports = {
