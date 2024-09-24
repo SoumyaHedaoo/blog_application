54e8c0700de5d2906f1721bee9aa315e5eb662c9
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-09-24 15:17:11 +0530

    make public folder static available

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.set("views" , path.resolve("./views"));
 
 app.use(express.urlencoded({extended : false}));
 app.use(cookieParser());
+app.use(express.static(path.resolve("./public")));
 app.use(checkAuthenticationCookie("token"));
 app.get("/" , displayHomePage);
 
