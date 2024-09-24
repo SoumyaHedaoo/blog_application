4c281504b69d1147bfe148687ae044aeb75cb44a
Soumya Hedaoo <soumyahedaoo77@gmail.com>
2024-09-24 14:39:56 +0530

    use multer to accept cover image

--- routes/blog.js
+++ routes/blog.js
@@ -1,9 +1,24 @@
 const { Router} = require("express");
+const multer = require("multer");
+const path= require ("path");
+
 const {CreateNewBlog , handleSubmitBlog} = require("../controllers/handleblogs")
 const router = Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`));
+    },
+    filename: function (req, file, cb) {
+        const filename = `${Date.now()}-${file.originalname}`;
+        cb(null , filename);
+    }
+  });
+
+  const upload = multer({ storage: storage });
+
 router.get("/add-new" , CreateNewBlog);
 
-router.post('/' , handleSubmitBlog);
+router.post('/' ,upload.single("thumbnailImage") ,handleSubmitBlog);
 
 module.exports = router;
